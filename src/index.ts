96d9cb272c47c7ac757da712219f2c1342b4e104
James Wallington <jameswallington11@gmail.com>
2025-01-06 18:35:17 +0000

    boilerplate

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ const app: Express = express();
 
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-  console.log("CMS is running on", process.env.PORT);
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log("CMS is running on", port);
 });
